37a60a76cf16c48e716cc13ca1862725cd032675
Luca Isidoro <apprendista44@gmail.com>
2020-09-11 16:21:13 +0200

    inserita delite

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -7,7 +7,14 @@
 @foreach ($cars as $car)
   <div>
     <a href="{{ route('cars.show', $car)}}" >{{$car->manifacturer}} {{ $car->engine}}</a>
+    <br>
     <a href="{{ route('cars.edit', $car) }}">Modifica</a>
+    <form action="{{ route('cars.destroy', $car) }}" method="post">
+					@csrf
+					@method('DELETE')
+
+					<input type="submit" value="Elimina post">
+				</form>
 
   </div>
 @endforeach
